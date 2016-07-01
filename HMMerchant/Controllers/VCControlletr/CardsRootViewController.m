6f03e3af0c6f768614f4514d1af3cf206b92ba3f
humeng12 <h13273809702@163.com>
2016-07-01 10:07:04 +0800

    20160701

--- HMMerchant/Controllers/VCControlletr/CardsRootViewController.m
+++ HMMerchant/Controllers/VCControlletr/CardsRootViewController.m
@@ -60,10 +60,15 @@
 
 #import "CheShiViewController.h"
 
+#import "TLMenuButtonView.h"
+
 @interface CardsRootViewController ()
 {
      UIImageView *headView;
+     BOOL _ISShowMenuButton;
 }
+
+@property (nonatomic, strong) TLMenuButtonView *tlMenuView ;
 
 @property (strong,nonatomic) NSArray *buttonBacImages;
 @property (strong,nonatomic) NSArray *LabelTitles;
@@ -85,7 +90,45 @@
 
 
      [self addButtonAndLabel];
+
+
+     UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(self.view.bounds.size.width-67, self.view.bounds.size.height-107, 55, 55)];
+     button.layer.cornerRadius = 27.5;
+     button.backgroundColor = [UIColor greenColor];
+     [button addTarget:self action:@selector(clickAddButton:) forControlEvents:UIControlEventTouchUpInside];
+     [button setImage:[UIImage imageNamed:@"jiahao"] forState:UIControlStateNormal];
+     [self.view addSubview:button];
+
+     TLMenuButtonView *tlMenuView =[TLMenuButtonView standardMenuView];
+     tlMenuView.centerPoint = button.center;
+     __weak typeof(self) weakSelf = self;
+     tlMenuView.clickAddButton = ^(NSInteger tag, UIColor *color){
+          weakSelf.view.backgroundColor = color;
+          _ISShowMenuButton = YES;
+          [weakSelf clickAddButton:button];
+     };
+     _tlMenuView = tlMenuView;
 }
+
+
+- (void)clickAddButton:(UIButton *)sender{
+
+     if (!_ISShowMenuButton) {
+          [UIView animateWithDuration:0.2 animations:^{
+               CGAffineTransform rotate = CGAffineTransformMakeRotation( M_PI / 4 );
+               [sender setTransform:rotate];
+          }];
+          [_tlMenuView showItems];
+     }else{
+          [UIView animateWithDuration:0.2 animations:^{
+               CGAffineTransform rotate = CGAffineTransformMakeRotation( 0 );
+               [sender setTransform:rotate];
+          }];
+          [_tlMenuView dismiss];
+     }
+     _ISShowMenuButton = !_ISShowMenuButton;
+}
+
 
 
 -(void)addButtonAndLabel
